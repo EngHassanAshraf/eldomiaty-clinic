9476470bfec43b40ef854ac45a31a8cdb9c92d64
Eldomiaty Clinic <dev@eldomiaty.clinic>
2026-04-12 23:48:17 +0200

    chore: replace favicon with transparent background version and add favicon variants

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ar" dir="rtl">
       <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link rel="icon" href="/favicon-transparence-bg.jpg" type="image/jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
